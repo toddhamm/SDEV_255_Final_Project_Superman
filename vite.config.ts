75a74e64681975f97fcb4cf413fafd99291bde5f
Todd Hamm <toddhamm30@gmail.com>
2026-09-25 11:19:08 -0400

    updated allowed hosts setting so vite will work on render.com

--- vite.config.ts
+++ vite.config.ts
@@ -4,4 +4,8 @@ import { defineConfig } from 'vite'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server: {
+    // Allows specific Render subdomains or all hosts
+    allowedHosts: ['.onrender.com', 'sdev-255-final-project-superman-ylkx.onrender.com'], 
+  },
 })
